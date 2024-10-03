cdea405ae0d66530565e9a2d07d60e40e72488a1
Swierszczykowaty <loolok1232@gmail.com>
2024-10-03 23:49:46 +0200

    [ADD] - tailwind + untils (not working)

--- surella/tailwind.config.js
+++ surella/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#050816",//todo
+        "black-100": "#100d25",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
