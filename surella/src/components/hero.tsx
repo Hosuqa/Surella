dd3f7c9d49560470d01347a51440b996c087a64a
Swierszczykowaty <loolok1232@gmail.com>
2024-10-05 15:19:09 +0200

    [ADD] - framer motion, fix styles.ts motion.ts

--- surella/src/components/hero.tsx
+++ surella/src/components/hero.tsx
@@ -1,7 +1,21 @@
+import {styles} from "../styles";
+import { fadeIn } from "../untils/motion";
+import { motion } from "framer-motion";
+
 const Hero = () => {
   return (
-    <div className="bg-slate-300 flex justify-center items-center w-screen h-20">Hero</div>
-  )
-}
+    <>
+      <div className="bg-slate-300 flex justify-center items-center w-screen py-20">
+        <motion.div variants={fadeIn()}
+        initial="hidden"
+        animate="show">
+          <h2 className={styles.sectionHeadText}>
+            Surella
+          </h2>
+        </motion.div>
+      </div>
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
---
+++ surella/src/untils/motion.ts
@@ -0,0 +1,87 @@
+export const textVariant = (delay: number = 0) => {
+  return {
+    hidden: {
+      y: -50,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 1.25,
+        delay: delay,
+      },
+    },
+  };
+};
+
+export const fadeIn = (direction: string = "right", delay: number = 0, duration: number = 0.5) => {
+  return {
+    hidden: {
+      x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const zoomIn = (delay: number = 0, duration: number = 0.5) => {
+  return {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: "tween",
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const slideIn = (direction: string = "right", delay: number = 0, duration: number = 0.5) => {
+  return {
+    hidden: {
+      x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+      y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      transition: {
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+//kawałek HOC section wrapper
+// export const staggerContainer = (staggerChildren, delayChildren) => {
+//   return {
+//     hidden: {},
+//     show: {
+//       transition: {
+//         staggerChildren: staggerChildren,
+//         delayChildren: delayChildren || 0,
+//       },
+//     },
+//   };
+// };
+  
