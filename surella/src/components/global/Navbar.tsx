ed647c24b4767b949857f4affbd7f47df7104e91
Swierszczykowaty <loolok1232@gmail.com>
2024-10-07 17:50:50 +0200

    [FIX] - Nav bar spacing (justify-around)

--- surella/src/components/global/Navbar.tsx
+++ surella/src/components/global/Navbar.tsx
@@ -7,39 +7,34 @@ type Props = {
     title: string,
 }
 
-const Title =({title}: Props) => {
-    return(
-      <>
+const Title = ({ title }: Props) => {
+    return (
         <div className={`${styles.NavbarText} text-surella-600 font-interExtraBold font-bold cursor-pointer uppercase`}>
             {title}
         </div>
-      </>
     );
-  };
-
+};
 
 const Navbar = () => {
     const [active, setActive] = useState('');
 
     return (
         <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-white shadow-md`}>
-            <div className="w-full flex max-w-7xl mx-auto items-center ">
-                    <Title title="O nas"/>
-                    <Title title="Realizacje"/>
-            </div>
-            <Link
-                to="/"
-                className="flex items-center gap-2"
-                onClick={()=>{
-                    setActive("");
-                    window.scrollTo(0,0);
+            <div className="w-full flex  items-center max-w-7xl justify-around mx-auto">
+                    <Title title="O nas" />
+                    <Title title="Realizacje" />
+                <Link
+                    to="/"
+                    className="flex items-center gap-2"
+                    onClick={() => {
+                        setActive("");
+                        window.scrollTo(0, 0);
                     }}>
                     <img src={surella} alt="logo" className="w-10 h-10 object-contain" />
                     <p className="text-[25px] text-surella-600 font-interExtraBold font-bold cursor-pointer">Surella</p>
-            </Link>
-            <div className="w-full flex max-w-7xl mx-auto items-center ">
-                <Title title="Współpraca"/>
-                <Title title="Kontakt"/>
+                </Link>
+                    <Title title="Współpraca" />
+                    <Title title="Kontakt" />
             </div>
         </nav>
     );
