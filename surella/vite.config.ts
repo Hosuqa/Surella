f0ac229df5a318215d18a87dfb2795c8e59c5dd5
Hosuqa <kuba.grabowski129@gmail.com>
2024-10-07 22:06:14 +0200

    [ADD] Aliases aka ścieżki z @

--- surella/vite.config.ts
+++ surella/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
-
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
