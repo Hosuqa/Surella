de568a94bb4982d9b10060266c812d2547e7de90
Swierszczykowaty <loolok1232@gmail.com>
2024-10-08 20:19:21 +0200

    [ADD] - fav icon, nav bar toggle menu broken

--- surella/src/components/global/Navbar.tsx
+++ surella/src/components/global/Navbar.tsx
@@ -1,47 +1,70 @@
+import { useState } from "react";
 import { styles } from "../../styles";
 import { Link } from "react-router-dom";
 import { surella } from '../../assets';
+import { motion } from "framer-motion";
 
 type Props = {
-    title: string,
-}
+    title: string;
+};
 
 const Title = ({ title }: Props) => {
     return (
-        <div className={`${styles.NavbarText} w-full hidden xl:flex justify-center text-surella-600 -tracking-tight font-interExtraBold font-bold cursor-pointer `}>
+        <div className={`${styles.NavbarText} w-full hidden xl:flex justify-center text-surella-600 -tracking-tight font-interExtraBold font-bold cursor-pointer`}>
             {title}
         </div>
     );
 };
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen((prev) => !prev);
+    };
 
     return (
+        <>
         <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-white uppercase`}>
-            <div className="w-full flex justify-between  items-center ">
-            <Link
+            <div className="w-full flex justify-between items-center">
+                <Link
                     to="/"
-                    className="flex xl:justify-sart gap-2 w-full  cursor-default"
-                    onClick={() => {
-                        window.scrollTo(0, 0);
-                    }}>
+                    className="flex xl:justify-start gap-2 w-full cursor-default"
+                    onClick={() => window.scrollTo(0, 0)}
+                >
                     <img src={surella} alt="logo" className="w-10 h-10 object-contain" />
-                    <p className="text-[25px] text-surella-600 font-interExtraBold font-bold cursor-pointer ">Surella</p>
+                    <p className="text-[25px] text-surella-600 font-interExtraBold font-bold cursor-pointer">Surella</p>
                 </Link>
-                <div className="justify-end flex gap-10 w-full">
+                <div className="justify-end hidden xl:flex gap-10 w-full">
                     <Title title="O nas" />
                     <Title title="Realizacje" />
-                
                     <Title title="Współpraca" />
                     <Title title="Kontakt" />
-                    </div>
-                    <div className="xl:hidden flex flex-col gap-2 bg cursor-pointer">
-                        <div className="h-1.5 w-12 bg-surella-600"></div>
-                        <div className="h-1.5 w-12 bg-surella-600"></div>
-                        <div className="h-1.5 w-12 bg-surella-600"></div>
-                    </div>
+                </div>
+                <div className="xl:hidden flex flex-col gap-[10px] cursor-pointer"
+                    onClick={toggleMenu}>
+                    <div className="h-[5px] w-12 bg-surella-600"></div>
+                    <div className="h-[5px] w-12 bg-surella-600"></div>
+                    <div className="h-[5px] w-12 bg-surella-600"></div>
+                </div>
             </div>
         </nav>
+        <motion.div
+            initial={{ y: "-100%" }}
+            animate={{ y: menuOpen ? 0 : "-100%" }}
+            transition={{ type: "tween", duration: 0.5, ease: "easeInOut" }}
+            className="fixed top-20 right-40 bg-surella-800 flex flex-col z-50 gap-5 p-10"
+        >
+            {menuOpen && (
+                <>
+                    <Link to="/" className="text-white font-bold">O nas</Link>
+                    <Link to="/realizacje" className="text-white font-bold">Realizacje</Link>
+                    <Link to="/wspolpraca" className="text-white font-bold">Współpraca</Link>
+                    <Link to="/kontakt" className="text-white font-bold">Kontakt</Link>
+                </>
+            )}
+        </motion.div>
+        </>
     );
 };
 
