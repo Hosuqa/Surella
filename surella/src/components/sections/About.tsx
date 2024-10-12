83fb744a606b1adcf00df2dc1a83caf8d523d5ec
Michał Świercz <loolok1232@gmail.com>
2024-10-12 16:49:39 +0200

    [FIX] - swiper bullet color

--- surella/src/components/sections/About.tsx
+++ surella/src/components/sections/About.tsx
@@ -1,11 +1,27 @@
 import Title from '../global/Title'
 import { XlWrapper } from '@components/global/Wrappers'
- 
+import { motion, useMotionValue, useTransform, animate } from "framer-motion";
+import { useEffect } from "react";
+
+
 const About = () => {
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, Math.round);
+
+  useEffect(() => {
+    const animation = animate(count, 5650, { duration: 2 });
+
+    return animation.stop;
+  }, []);
+
   return (
     <XlWrapper vertical>
       <div className="bg-white flex h-screen flex-col gap-10">
         <Title title='Poznaj Surelle' subtitle='Trochę o nas'></Title>
+        <motion.div className='flex justify-center text-4xl'>
+          <motion.h1>{rounded}</motion.h1>
+        </motion.div>
+          
         <Title title='Poznaj naszą firmę' subtitle='O nas'></Title>
         <Title title='AW JNAFDBJA AKWNE A KAND' subtitle='ADDADDA AF A FAF A'></Title>
 
