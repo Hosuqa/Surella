f344db99d31f267123293bd12a4460e90644862d
Swierszczykowaty <loolok1232@gmail.com>
2024-10-16 23:39:52 +0200

    [ADD] - Hero fix
    Co-authored-by: Hosuqa <Hosuqa@users.noreply.github.com>

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -8,27 +8,27 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 // bg-surella-500 ===> HERO main div
-
+//bg-gradient-to-tr from-teal-900 via-surella-500 to-teal-600 animated-background
 const Hero = () => {
   return (
     <>
-      <div className="flex xl:flex-row flex-col font-interExtraBold h-[90vh] xl:h-[95vh] bg-gradient-to-tr from-teal-900 via-surella-500 to-teal-600 animated-background">
-        <div className={`${styles.paddingX} flex flex-col w-full h-1/2 xl:w-2/5 xl:h-full justify-center mt-[15%] lg:mt-[10%]`}>
-          <h2 className={`${styles.heroHeadText} text-white tracking-wide font-semibold uppercase`}>
+      <div className="flex lg:flex-row flex-col font-interExtraBold h-[calc(85vh)] mt-[80px] bg-surella-500">
+        <div className={`${styles.paddingX} flex lg:shrink-0 xl:shrink flex-col w-full lg:w-[550px] xl:w-[660px] h-1/2 lg:h-full lg:justify-end justify-center lg:pb-[200px]`}>
+          <h2 className={`${styles.heroHeadText} text-white font-semibold uppercase`}>
             Surella.pl
           </h2>
-          <p className={`${styles.heroSubText} text-white text-nowrap -translate-y-3`}> 
+          <p className={`${styles.heroSubText} text-white text-nowrap -translate-y-2 md:-translate-y-3`}> 
             Tworzymy przestrzeń dla Twojego komfortu.
           </p>
           <motion.div className="w-fit h-fit bg-white mt-2"
             whileHover={{ scale: 1.05 }}>
-              <motion.p className={`${styles.heroButton} text-center text-surella-500 font-interExtraBold font-bold px-16 py-2 md:py-3 cursor-pointer uppercase text-nowrap`}
+              <motion.p className={`${styles.heroButton} text-center text-surella-500 font-interExtraBold font-bold px-16 py-3 cursor-pointer uppercase text-nowrap`}
                > 
                 Sprawdź wycenę
               </motion.p>
           </motion.div>
         </div>
-        <div className="w-full h-1/2 xl:w-3/5 xl:h-full border-t-[8px] xl:border-l-[12px] border-white">
+        <div className="overflow-hidden flex-1 w-full h-1/2 lg:w-3/5 lg:h-full border-t-[8px] lg:border-t-[0px] lg:border-l-[12px] border-white">
         <Swiper pagination={{ dynamicBullets: true, }} modules={[Pagination, Autoplay]} autoplay={{ delay: 3500, disableOnInteraction: false,}} loop={true}  className="h-full w-full ">
           <SwiperSlide>
             <img src={hero1} className="object-cover h-full w-full" alt="hero" />
@@ -47,3 +47,27 @@ const Hero = () => {
 };
 
 export default Hero;
+
+{/*
+<div className="flex flex-col xl:flex-row bg-white w-full h-[calc(95vh-96px)] mt-24 relative gap-2 overflow-hidden">
+  <div className="w-full xl:w-[900px] shrink-0 grow-0 bg-surella-500 flex flex-col justify-center py-24">
+    <div className="sm:pl-24 md:pl-32 lg:pl-48 pl-8">
+      <h2 className="text-white uppercase font-[700] text-7xl mb-2">Surella.pl</h2>
+      <h4 className="text-white font-[400] text-md xl:text-2xl mb-10">Tworzymy przestrzeń dla Twojego komfortu</h4>
+      <div className="bg-white px-10 py-4 uppercase w-fit font-[700] text-surella-500 text-sm xl:text-xl">Sprawdź wycenę</div>
+    </div>
+  </div>
+  <div className="w-full bg-surella-500 relative overflow-hidden min-h-[400px]">
+    <Swiper pagination={{ dynamicBullets: true, }} modules={[Pagination, Autoplay]} autoplay={{ delay: 3500, disableOnInteraction: false,}} loop={true}  className="h-full w-full">
+      <SwiperSlide>
+        <img src={hero1} className="object-cover h-full w-full" alt="hero" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={hero2} className="object-cover h-full w-full" alt="hero" />
+      </SwiperSlide>
+      <SwiperSlide>  
+        <img src={hero3} className="object-cover h-full w-full" alt="hero" />
+      </SwiperSlide>
+    </Swiper>
+  </div>
+</div>  */}
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,5 @@
 const styles = {
-    paddingX: "sm:px-16 px-6",
+    paddingX: "px-6 sm:px-16 md:px-20 lg:px-24 lg:px-26 xl:px-28",
     paddingHeroX: " px-6 sm:px-10 md:px-16 lg:px-24 xl:px-28 2xl:px-56",
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-16 py-10",
@@ -7,11 +7,11 @@ const styles = {
     NavbarText:
     " md:text-[17px] sm:text-[15px] text-[13px]",
     heroHeadText:
-      "xl:text-[70px] lg:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+      "xl:text-[70px] sm:text-[50px] text-[40px]",
     heroSubText:
-      "xl:text-[22px] lg:text-[20px] xs:text-[18px] text-[10px]",
+      "xl:text-[22px] text-[18px]",
     heroButton:
-      "xl:text-[24px] lg:text-[20px] md:text-[18px] xs:text-[16px] text-[12px]",
+      "xl:text-[24px]  md:text-[18px] text-[16px]",
     aboutText:
       "xl:text-[22px] xs:text-[18px] text-[10px]",
 
