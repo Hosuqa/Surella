c9acd55a22a0a5956a889f953cddd45c52cd91c7
Hosuqa <kuba.grabowski129@gmail.com>
2024-10-16 23:05:51 +0200

    Update ContentBox.tsx

--- src/components/global/ContentBox.tsx
+++ src/components/global/ContentBox.tsx
@@ -2,11 +2,12 @@ import Navbar from '@components/global/Navbar';
 import Hero from '@components/sections/Hero';
 import About from '@components/sections/About';
 import Counter from '@components/sections/Counter';
-import Gallery from '@components/sections/Gallery';
+
+
 const ContentBox = () => {
     return (
         <>
-            <Navbar/><Hero/><About/><Gallery/><Counter/>
+            <Navbar/><Hero/><About/><Counter/>
         </>
     );
 }
