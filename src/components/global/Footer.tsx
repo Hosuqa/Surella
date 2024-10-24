9e37005372ac2f1a4393af6da8231949fec75a17
Michał Świercz <loolok1232@gmail.com>
2024-10-24 14:30:28 +0200

    [ADD] - Counter without responsive

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <>
             <footer className='bg-surella-500 h-full w-full flex flex-col items-center' >
                 <img src={surella} alt="logo" className="w-14 h-14 m-10 object-contain" />
-                <p className='text-white mb-10'>&copy; {new Date().getFullYear()} Surella. Wszystkie prawa zastrzeżone.</p>
+                <p className='text-white mb-10'>Copyright &copy; {new Date().getFullYear()} Surella</p>
             </footer>
         </>
     );
--- src/components/sections/Counter.tsx
+++ src/components/sections/Counter.tsx
@@ -20,7 +20,7 @@ const Counter = () => {
 
   useEffect(() => {
     if (inView) {
-      const targetValues = [536, 2009, 7245]; 
+      const targetValues = [3536, 16, 7245]; 
       counts.forEach((count, index) => {
         animate(count, targetValues[index], { 
           duration: 3.5,
@@ -30,14 +30,32 @@ const Counter = () => {
     }
   }, [inView, counts]);
 
+  const items = [
+    { icon: <FaBuilding />, text: "Zadowolonych klientów" },
+    { icon: <FaPeopleGroup />, text: "Doświadczenia na rynku" },
+    { icon: <LuBlinds />, text: "Zamontowanych rolet" },
+  ];
+
   return (
     <XlWrapper vertical>
-      <div className="w-full h-full bg-slate-200 flex items-center justify-center">
-        <motion.div ref={ref} className="flex gap-10 ">
-          {roundedCounts.map((rounded, index) => (
-            <motion.h1 key={index} className="text-4xl font-bold flex justify-center">{rounded}</motion.h1>
-          ))}
-        </motion.div>
+      <div ref={ref} className="w-full h-full flex justify-around">
+        {roundedCounts.map((rounded, index) => (
+          <div className="flex flex-col items-center" key={index}>
+            <div className="w-40 h-40 bg-surella-700 aspect-square flex items-center justify-center rotate-45">
+              <div className="-rotate-45 text-white w-full h-full flex items-center justify-center text-7xl">
+                {items[index].icon}
+              </div>
+            </div>
+            <div className=" w-32 flex flex-col justify-center items-center h-full mt-14">
+              <motion.h1 className="text-4xl font-bold flex justify-center text-surella-700">
+                {rounded}
+              </motion.h1>
+              <p className="text-surella-700 text-center text-wrap mt-1 uppercase text-lg font-semibold">
+                {items[index].text}
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
     </XlWrapper>
   );
