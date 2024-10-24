e953f65904764e5cb07b3399160832e2d397849b
Swierszczykowaty <loolok1232@gmail.com>
2024-10-24 22:18:26 +0200

    [FIX] - white surella

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,11 @@
 import surella from "./surella.svg"
+import surellawhite from "./surellawhite.svg"
 import hero1 from "./hero1.jpg"
 import hero2 from "./hero2.jpg"
 import hero3 from "./hero3.jpg"
 
 export{
+    surellawhite,
     surella,
     hero1,
     hero2,
--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,11 +1,11 @@
-import { surella } from '../../assets';
+import { surellawhite } from '../../assets';
 
 
 const Footer = () => {
     return (
         <>
             <footer className='bg-surella-500 h-full w-full flex flex-col items-center' >
-                <img src={surella} alt="logo" className="w-14 h-14 m-10 object-contain" />
+                <img src={surellawhite} alt="logo" className="w-14 h-14 m-10 object-contain" />
                 <p className='text-white mb-10'>Copyright &copy; {new Date().getFullYear()} Surella</p>
             </footer>
         </>
