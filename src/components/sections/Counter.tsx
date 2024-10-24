53315fa81d2425f6f114ca622d0fa60934fd1669
Michał Świercz <loolok1232@gmail.com>
2024-10-24 18:08:04 +0200

    [ADD] - React Countup

--- src/components/sections/Counter.tsx
+++ src/components/sections/Counter.tsx
@@ -1,61 +1,63 @@
-import { motion, useMotionValue, useTransform, animate, useInView } from "framer-motion";
-import { useEffect, useRef } from "react";
 import { LgWrapper } from '@components/global/Wrappers'
 import { FaBuilding } from "react-icons/fa6";
 import { FaPeopleGroup } from "react-icons/fa6";
 import { LuBlinds } from "react-icons/lu";
-
-
+import CountUp from 'react-countup';
 
 const Counter = () => {
-  const counts = [
-    useMotionValue(0),
-    useMotionValue(0),
-    useMotionValue(0),
-  ];
-  
-  const roundedCounts = counts.map(count => useTransform(count, Math.round));
-  const ref = useRef(null);
-  const inView = useInView(ref, { once: true });
-
-  useEffect(() => {
-    if (inView) {
-      const targetValues = [3536, 16, 7245]; 
-      counts.forEach((count, index) => {
-        animate(count, targetValues[index], { 
-          duration: 3.5,
-          ease: [0, 0, 0, 1]
-        });
-      });
-    }
-  }, [inView, counts]);
-
-  const items = [
-    { icon: <FaBuilding />, text: "Zadowolonych klientów" },
-    { icon: <FaPeopleGroup />, text: "Doświadczenia na rynku" },
-    { icon: <LuBlinds />, text: "Zamontowanych rolet" },
-  ];
 
   return (
     <LgWrapper vertical>
-      <div ref={ref} className="w-full h-full flex justify-around">
-        {roundedCounts.map((rounded, index) => (
-          <div className="flex flex-col items-center" key={index}>
+      <div className="w-full h-full flex justify-around">
+          <div className="flex flex-col items-center" >
             <div className="w-[70px] sm:w-28 lg:w-40 bg-surella-600 aspect-square flex items-center justify-center rotate-45">
               <div className="-rotate-45 text-white w-full h-full flex items-center justify-center text-4xl sm:text-6xl lg:text-7xl">
-                {items[index].icon}
+                <FaPeopleGroup />
               </div>
             </div>
             <div className="sm:w-32 flex flex-col justify-center items-center h-full mt-8 md:mt-12 lg:mt-14">
-              <motion.h1 className="text-2xl md:text-3xl lg:text-4xl font-bold flex justify-center text-surella-700">
-                {rounded}
-              </motion.h1>
+              <h1 className="text-nowrap text-2xl md:text-3xl lg:text-4xl font-bold flex justify-center text-surella-700">
+                <CountUp start={0} end={6038} duration={3.5} enableScrollSpy={true} scrollSpyOnce={true} scrollSpyDelay={0.4} separator=","/>
+              </h1>
               <p className="text-surella-600 text-center text-wrap mt-2 uppercase text-[12px] md:text-[14px] lg:text-lg font-semibold">
-                {items[index].text}
+                Zadowolonych klientow
               </p>
             </div>
           </div>
-        ))}
+
+          <div className="flex flex-col items-center" >
+            <div className="w-[70px] sm:w-28 lg:w-40 bg-surella-600 aspect-square flex items-center justify-center rotate-45">
+              <div className="-rotate-45 text-white w-full h-full flex items-center justify-center text-4xl sm:text-6xl lg:text-7xl">
+                <FaBuilding />
+              </div>
+            </div>
+            <div className="w-32 flex flex-col justify-center items-center h-full mt-8 md:mt-12 lg:mt-14">
+              <h1 className="text-nowrap text-2xl md:text-3xl lg:text-4xl font-bold flex justify-center text-surella-700">
+                <CountUp start={0} end={16} duration={6} enableScrollSpy={true} scrollSpyOnce={true} scrollSpyDelay={0.4} separator=","/>
+              </h1>
+              <p className="text-surella-600 text-center text-wrap mt-2 uppercase text-[12px] md:text-[14px] lg:text-lg font-semibold">
+                Lat Doświadczenia
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center" >
+            <div className="w-[70px] sm:w-28 lg:w-40 bg-surella-600 aspect-square flex items-center justify-center rotate-45">
+              <div className="-rotate-45 text-white w-full h-full flex items-center justify-center text-4xl sm:text-6xl lg:text-7xl">
+                <LuBlinds />
+              </div>
+            </div>
+            <div className="sm:w-32 flex flex-col justify-center items-center h-full mt-8 md:mt-12 lg:mt-14">
+              <h1 className="text-nowrap text-2xl md:text-3xl lg:text-4xl font-bold flex justify-center text-surella-700">
+                <CountUp start={0} end={10346} duration={3.5} enableScrollSpy={true} scrollSpyOnce={true} scrollSpyDelay={0.4} separator="," useEasing={true} />
+                {/* easingFn={"easeOutCubic"} */}
+              </h1>
+              <p className="text-surella-600 text-center text-wrap mt-2 uppercase text-[12px] md:text-[14px] lg:text-lg font-semibold">
+                Zamontowanych rolet
+              </p>
+            </div>
+          </div>
+        
       </div>
     </LgWrapper>
   );
