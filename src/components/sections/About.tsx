9e4345ca3bb7162456d85c80576fcf376885fc9d
Swierszczykowaty <loolok1232@gmail.com>
2024-11-03 13:14:24 +0100

    [INSTALL] - GSAP

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,60 +1,91 @@
-import Title from '../global/Title'
-import { XlWrapper } from '@components/global/Wrappers'
+import Title from '../global/Title';
+import { XlWrapper } from '@components/global/Wrappers';
 import { styles } from "../../styles";
-//icons
 import { FaToolbox, FaHandshake, FaMoneyCheckDollar } from "react-icons/fa6"; 
 import { FaTools } from "react-icons/fa";
+import React, { ReactElement, useEffect } from 'react';
+import texts from '../../texts.json';
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import React, { ReactElement } from 'react';
-import texts from '../../texts.json'
-import { motion } from "framer-motion";
-import { fadeIn } from "../../untils/motion";
+gsap.registerPlugin(ScrollTrigger);
 
+type Props = {
+  title: string;
+  icon: ReactElement;
+};
 
- type Props = {
-   title: string;
-   icon: ReactElement;
- };
- //react-parallax-tilt = > zmiana div na Titlr + zmienne + plik About.css z paralax_out paralax_in
 const IconBox = ({title, icon}: Props) => {
   return (
-    <div
-    className='bg-surella-600 text-white w-full h-full min-w-[160px] flex flex-col justify-center items-center p-6'>
+    <div className='icon-box bg-surella-600 text-white w-full h-full min-w-[160px] flex flex-col justify-center items-center p-6'>
       {React.cloneElement(icon, { className: 'md:w-16 md:h-16 w-10 h-10' })}
       <p className='pt-4 uppercase font-bold text-[18px] tracking-wider'>{title}</p>
     </div>
   );
-}
+};
 
 const About = () => {
   const description = texts[0]?.companyDescription;
 
+  useEffect(() => {
+    gsap.fromTo(
+      ".icon-box",
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        stagger: 0.25,
+        scrollTrigger: {
+          trigger: ".description",
+          start: "top 90%",
+          toggleActions: "play reverse play reverse",
+        },
+      }
+    );
+
+    gsap.fromTo(
+      ".description",
+      { opacity: 0, x: -100 },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".description",
+          start: "top 90%",
+          toggleActions: "play reverse play reverse",
+        },
+      }
+    );
+  }, []);
 
   return (
-    <XlWrapper id='About' vertical >
-      <div  className="bg-white w-full flex-col">
+    <XlWrapper id='About' vertical>
+      <div className="bg-white w-full flex-col">
         <Title title='Poznaj Surelle' subtitle='Trochę o nas'></Title>
         <div className='flex xl:flex-row flex-col w-full gap-4'>
-          <motion.div
-          variants={fadeIn("",0.4,0.5)}  initial="hidden"  animate="show"
-          className='w-full xl:w-3/5 h-full flex order-2 xl:order-1'>
+          <div className='w-full xl:w-3/5 h-full flex order-2 xl:order-1 description'>
             <div className='w-full h-full bg-slate-100 font-[400] p-16'>
-              <p className={`${styles.aboutText} tracking-wide text-surella-700 leading-relaxed text-justify`}>{description}</p>
+              <p className={`${styles.aboutText} tracking-wide text-surella-700 leading-relaxed text-justify`}>
+                {description}
+              </p>
             </div>
-          </motion.div>
-          <motion.div className='w-full xl:w-2/5 gap-4 flex flex-col items-center justify-center xl:order-2 order-1'>
+          </div>
+          <div className='w-full xl:w-2/5 gap-4 flex flex-col items-center justify-center xl:order-2 order-1'>
             <div className='flex h-full w-full gap-4'>
-              <IconBox title='Ekspertyza' icon={<FaToolbox/>}/>
-              <IconBox title='Montaż' icon={<FaTools />}/>
+              <IconBox title='Ekspertyza' icon={<FaToolbox />} />
+              <IconBox title='Montaż' icon={<FaTools />} />
             </div>
             <div className='flex h-full w-full gap-4'>
-              <IconBox title='Doradztwo' icon={<FaHandshake />}/>
-              <IconBox title='Sprzedaż' icon={<FaMoneyCheckDollar />}/>
+              <IconBox title='Doradztwo' icon={<FaHandshake />} />
+              <IconBox title='Sprzedaż' icon={<FaMoneyCheckDollar />} />
             </div>
-          </motion.div>
+          </div>
         </div>
       </div>
     </XlWrapper>
-  )
-}
-export default About
+  );
+};
+
+export default About;
