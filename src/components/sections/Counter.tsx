3539c679dfa8408cb71257d2ad6da91a7546d92b
Michał Świercz <loolok1232@gmail.com>
2024-11-05 09:36:44 +0100

    [ADD] - Counter gsap

--- src/components/sections/Counter.tsx
+++ src/components/sections/Counter.tsx
@@ -1,15 +1,38 @@
+import { useEffect } from 'react';
 import { CounterWrapper } from '@components/global/Wrappers'
 import { FaBuilding } from "react-icons/fa6";
 import { FaPeopleGroup } from "react-icons/fa6";
 import { LuBlinds } from "react-icons/lu";
 import CountUp from 'react-countup';
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Counter = () => {
+
+  useEffect(() => {
+    gsap.fromTo(
+      ".sqare",
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        stagger: 0.25,
+        scrollTrigger: {
+          trigger: ".sqare",
+          start: "top 90%",
+          toggleActions: "play none none none",
+        },
+      }
+    );
+  }, []);
 
   return (
     <CounterWrapper vertical className='bg-surella-600'>
       <div className="w-full h-full flex justify-around">
-          <div className="flex flex-col items-center" >
+          <div className="sqare flex flex-col items-center" >
             <div className="w-[70px] sm:w-28 lg:w-40 bg-white aspect-square flex items-center justify-center rotate-45 hover:scale-110 duration-300">
               <div className="-rotate-45 text-surella-600 w-full h-full flex items-center justify-center text-4xl sm:text-6xl lg:text-7xl ">
                 <FaPeopleGroup />
@@ -17,7 +40,7 @@ const Counter = () => {
             </div>
             <div className="md:w-32 w-28 flex flex-col justify-center items-center h-full mt-8 md:mt-12 lg:mt-14">
               <h1 className="text-nowrap text-xl md:text-3xl lg:text-4xl font-bold flex justify-center text-white">
-                <CountUp start={0} end={3839} duration={3.5} enableScrollSpy={true} scrollSpyOnce={true} scrollSpyDelay={0.4} separator=","/>
+                <CountUp start={0} end={3839} duration={4.5} delay={1.5} enableScrollSpy={true} scrollSpyOnce={true} scrollSpyDelay={0.4} separator=","/>
               </h1>
               <p className="text-white text-center text-wrap mt-2 uppercase text-[11px] md:text-[14px] lg:text-lg font-semibold">
                 Zadowolonych klientow
@@ -25,7 +48,7 @@ const Counter = () => {
             </div>
           </div>
 
-          <div className="flex flex-col items-center" >
+          <div className="sqare flex flex-col items-center" >
             <div className="w-[70px] sm:w-28 lg:w-40 bg-white aspect-square flex items-center justify-center rotate-45 hover:scale-110 duration-300">
               <div className="-rotate-45 text-surella-600 w-full h-full flex items-center justify-center text-4xl sm:text-6xl lg:text-7xl ">
                 <FaBuilding />
@@ -33,7 +56,7 @@ const Counter = () => {
             </div>
             <div className="md:w-32 w-28 flex flex-col justify-center items-center h-full mt-8 md:mt-12 lg:mt-14">
               <h1 className="text-nowrap text-xl md:text-3xl lg:text-4xl font-bold flex justify-center text-white">
-                <CountUp start={0} end={16} duration={6} enableScrollSpy={true} scrollSpyOnce={true} scrollSpyDelay={0.4} separator=","/>
+                <CountUp start={0} end={16} delay={1.75} duration={6} enableScrollSpy={true} scrollSpyOnce={true} scrollSpyDelay={0.4} separator=","/>
               </h1>
               <p className="text-white text-center text-wrap mt-2 uppercase text-[11px] md:text-[14px] lg:text-lg font-semibold">
                 Lat Doświadczenia
@@ -41,7 +64,7 @@ const Counter = () => {
             </div>
           </div>
 
-          <div className="flex flex-col items-center" >
+          <div className="sqare flex flex-col items-center" >
             <div className="w-[70px] sm:w-28 lg:w-40 bg-white aspect-square flex items-center justify-center rotate-45 hover:scale-110 duration-300">
               <div className="-rotate-45 text-surella-600 w-full h-full flex items-center justify-center text-4xl sm:text-6xl lg:text-7xl ">
                 <LuBlinds />
@@ -49,7 +72,7 @@ const Counter = () => {
             </div>
             <div className="md:w-32 w-28 flex flex-col justify-center items-center h-full mt-8 md:mt-12 lg:mt-14">
               <h1 className="text-nowrap text-xl md:text-3xl lg:text-4xl font-bold flex justify-center text-white">
-                <CountUp start={0} end={6341} duration={3.5} enableScrollSpy={true} scrollSpyOnce={true} scrollSpyDelay={0.4} separator="," useEasing={true} />
+                <CountUp start={0} end={6341} delay={2} duration={4.5} enableScrollSpy={true} scrollSpyOnce={true} scrollSpyDelay={0.4} separator="," useEasing={true} />
                 {/* easingFn={"easeOutCubic"} */}
               </h1>
               <p className="text-white text-center text-wrap mt-2 uppercase text-[11px] md:text-[14px] lg:text-lg font-semibold">
